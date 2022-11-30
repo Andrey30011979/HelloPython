7d9c6d1b47529c7e967f6002d9c8e93dea9438c3
Andrey30011979 <109824571+Andrey30011979@users.noreply.github.com>
2022-11-30 11:48:25 +0300

    modifie lec

--- lec.py
+++ lec.py
@@ -1,1 +1,38 @@
-print('Hello world')
+# print('Hello world')
+# Переменные
+# Типы данных справедливы
+# int, float, boolean, str
+# list и др.
+# Python – язык с динамической типизацией
+# value = 2809
+# name = 'Sergey'
+
+# value = None
+# print(type(value))
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value  = 12334
+# print(type(value))
+
+# a = 123
+# b = 1.23
+# s = 'Hello world'
+# # s = "Hello 'world"
+# # s = 'Hello "world'    
+# # s = 'Hello \nworld'   #перенос строки \n
+# # print(s) #вывод строки
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+# print('{1} - {2} - {0}'.format(a, b, s))
+
+# f = True
+# print(f)
+# f = False
+# print(f)
+
+# list = []
+list = ['1', '2', '3', 'hello world']
+print(list)
