e35847cd7cff5a0e8dd195bfaf4148ba04cd596c
Andrey30011979 <109824571+Andrey30011979@users.noreply.github.com>
2022-11-30 13:39:24 +0300

    modifie lec

--- lec.py
+++ lec.py
@@ -34,5 +34,93 @@
 # print(f)
 
 # list = []
-list = ['1', '2', '3', 'hello world']
-print(list)
+# list = ['1', '2', '3', 'hello world']
+# print(list)
+
+# list = ['1', '2', '3']
+# col = ['hello', 1,2,4.5,True]
+# print(list)
+# print(col)
+ 
+# print('Введите a');
+# a = input()
+# print('Введите b');
+# b = input()
+# print(a, b)
+
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
+
+# print('Введите a');
+# a = int(input())
+# print('Введите b');
+# b = int(input())
+# print(a, '+', b, '=', a+b)
+
+# print('Введите a');
+# a = float(input())
+# print('Введите b');
+# b = float(input())
+# print(a, '+', b, '=', a+b)
+
+# Арифметические операции
+# Важно и нужно, без них вы не напишете ни одной программы
+# Если помните математику – проблем не будет
+# +, -,*, /, %, //,**
+# Приоритет операций
+# **, ⊕, ⊖,*, /, //, %, +, -
+# ( ) Скобки меняют приоритет
+
+# a = 123
+# b = -321
+# c = a + b
+# print(c)
+
+# a = 2
+# b = 8
+# c = a - b
+# print(c)
+
+# a = 2
+# b = 8
+# c = a * b
+# print(c)
+
+# a = 2
+# b = 8
+# c = a / b
+# print(c)
+
+# a = 12
+# b = 5
+# c = a // b # деление в целых числах
+# print(c)
+
+# a = 12
+# b = 8
+# c = a % b # остаток от деления
+# print(c)
+
+# a = 2
+# b = 800
+# c = a ** b # возведение в степень
+# print(c)
+
+# a = 1.3123
+# b = 3
+# c = round(a * b, 5)
+# print(c)
+
+# Операции присваивания
+# a = 3
+# a += 5 # a = a + 5
+# print(a)
+
+# a = 3
+# a *= 5
+# print(a)
+
+# Логические операции
+# >, >=, <, <=, ==, !=
+# not, and, or – не путать с &, |, ^
+# Кое-что ещё: is, is not, in, not in
