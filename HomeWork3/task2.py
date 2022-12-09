2bf508ebf5790a10a8d93f01763c04a1fb3bd8cf
Andrey30011979 <109824571+Andrey30011979@users.noreply.github.com>
2022-12-09 11:05:06 +0300

    Home Work 3

--- HomeWork3/task2.py
+++ HomeWork3/task2.py
@@ -4,5 +4,13 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+
+lst = [2, 3, 4, 5, 6] # lst = list(map(int, input("Введите числа через пробел:\n").split()))
+mult_lst(lst)         # mult_lst(lst)
+
 
 
--- HomeWork3/task5.py
+++ HomeWork3/task5.py
@@ -5,3 +5,18 @@
 # 3, 5, 8, 13, 21] [Негафибоначчи](https://ru.wikipedia.org/wiki/
 # Негафибоначчи#:~:text=В математике%2C числа негафибоначчи — отрицательно 
 # индексированные элементы последовательности чисел Фибоначчи.)
+
+n = int(input('Введите число: '))
+negofibonacci = [1,-1]
+fibonacci = [1,1]
+
+for i in range(2,n):
+    list = fibonacci[i-1]+fibonacci[i-2]
+    fibonacci.append(list)
+    list2 = negofibonacci[i-2] - negofibonacci[i-1]
+    negofibonacci.append(list2)
+
+negofibonacci.reverse()
+fibonacci.insert(0, 0)
+
+print(f' Для {n} => {negofibonacci+fibonacci}')
